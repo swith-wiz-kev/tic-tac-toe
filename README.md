6a872d7fbe862b4736786381ab2c2d55b7efec62
kev <ddrain10@gmail.com>
2023-08-09 05:00:47 +0800

    Add display functions

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Live Preview: https://swith-wiz-kev.github.io/tic-tac-toe/
 In this project, the UI makes use of simple buttons and radio buttons to modify the settings, reset, and trigger some game functionalities. There is also an edit button for changing a text to an input field and modifying its value as an input field. A few text elements will be changed, deleted or created as the game progresses.
 
 init module
+gameBoard Factory
 fillCell function
+display Factory
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const initGame = (() => {
   const addButtonsFunction = () => {
     function addButtonsFunction(button) {
@@ -58,6 +59,7 @@ const initGame = (() => {
 })();
 
 const gameBoard = (() => {
+  let moveNumber = 1;
   let boardContent = [];
   for (let index = 0; index < 9; index++) {
     boardContent[index] = "";
@@ -71,20 +73,66 @@ const gameBoard = (() => {
     boardContent[cellNumber - 1] = currentMarker;
   }
 
+  function isEnd() {}
+
+  function showEndMessage() {}
+
   const handleMove = (cellNumber) => {
-    if (boardContent[cellNumber - 1] === "") {
+    if (boardContent[cellNumber - 1] === "" && !isEnd()) {
       fillCell(cellNumber, currentMarker);
       gameBoard.handleMoveEnd();
     }
   };
 
-  const handleMoveEnd = () => {};
+  const handleMoveEnd = () => {
+    if (isEnd()) {
+      displayWinMsg.toggleText();
+      displayGameStatus.updateText();
+    }
+    moveNumber++;
+    displayGameStatus.updateText();
+  };
 
   return {
     handleMove,
     handleMoveEnd,
   };
 })();
+
+const display = (className) => {
+  const displayElement = document.querySelector(className);
+  const currentState =
+    getComputedStyle(displayElement).getPropertyValue("display");
+
+  const updateText = (text) => {
+    displayElement.textContent = text;
+  };
+
+  const toggleText = () => {
+    if (currentState === "none") {
+      displayElement.style.display = "block";
+    } else {
+      displayElement.style.display = "none";
+    }
+  };
+
+  const getText = () => {
+    return displayElement.textContent;
+  };
+
+  return {
+    updateText,
+    toggleText,
+    getText,
+  };
+};
+
+const displayGameStatus = display(".tictactoe-status");
+const displayWinMsg = display(".displaywinnerbg");
+const displayP1Name = display(".setnames.playerone>span");
+const displayP2Name = display(".setnames.playertwo>span");
+const displayP1Turns = display(".turns.playerone");
+const displayP2Turns = display(".turns.playertwo");
 
 //init
 initGame.addButtonsFunction();
